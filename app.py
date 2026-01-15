95e31982814f049dc7f1801108b11df3f24a325a
lucianorobertino97-blip <lucianorobertino97@gmail.com>
2026-01-15 18:34:40 -0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+
 
 from matplotlib.patches import FancyBboxPatch
 from matplotlib import colors
